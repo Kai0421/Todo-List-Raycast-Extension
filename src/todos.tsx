26d3d8b7c4ec019fb51f00d7dcd2dd29497d8582
Kai0421 <kaitamkai@gmail.com>
2022-09-06 12:09:07 -0400

    add List

--- src/todos.tsx
+++ src/todos.tsx
@@ -1,5 +1,23 @@
-import { Detail } from "@raycast/api";
+import { List, Icon, Color } from "@raycast/api";
+import MockData from '../mock_data/MockTodos'
 
 export default function Command() {
-    return (<Detail markdown={"Hello World"}/>)
+    return (
+        <List isShowingDetail>
+			{
+				MockData &&
+				MockData.map( ({title, isCompleted, description}) => (
+					<List.Item 
+						title={title}
+						icon={ isCompleted ? { source: Icon.CircleFilled, tintColor: Color.Green } : { source: Icon.Circle, tintColor: Color.Yellow}}
+						detail={
+							<List.Item.Detail 
+								markdown={description}
+							/>
+						}
+					/>
+				) )
+			}
+        </List>
+    )
 }
